6e2c9760e401f811a2365f498971d808cbbe13d0
Ian <mpawesome626@gmail.com>
2021-05-08 11:01:53 -0700

    second commit

---
+++ server/post/src/main/java/com/postserver/post/Post/Post.java
@@ -0,0 +1,39 @@
+package com.postserver.post.Post;
+
+public class Post {
+private String sender;
+private String title;
+private String body;
+public Post(String s, String t, String b) {
+this.sender = s;
+this.title = t;
+this.body = b;
+}
+public String getBody() {
+    return body;
+}
+public String getSender() {
+    return sender;
+}
+public String getTitle() {
+    return title;
+}
+public void setBody(String body) {
+    this.body = body;
+}
+public void setSender(String sender) {
+    this.sender = sender;
+}
+public void setTitle(String title) {
+    this.title = title;
+}
+@Override
+public String toString() {
+return "Title: " + this.title + "\n" +
+"Body:" + this.body + "\n" +
+"Sent by:" + this.sender + "\n";
+
+
+}
+
+}
